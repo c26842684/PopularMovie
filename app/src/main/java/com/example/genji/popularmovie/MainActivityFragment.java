c65413021e532de5f5a5a2836d34ba37722bed8a
c26842684 <d26842684@hotmail.com>
2016-10-16 19:29:20 -0400

    dummy data, customize arrayAdapter

--- app/src/main/java/com/example/genji/popularmovie/MainActivityFragment.java
+++ app/src/main/java/com/example/genji/popularmovie/MainActivityFragment.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    public Poster[] list = {
+            new Poster(R.drawable.cupcake,"dianying","2015-1-1",3.5,"good movie"),
+            new Poster(R.drawable.icecream,"dianying2","2015-1-1",3.5,"good movie")
+    };
+
+    public PosterAdapter mPosterAdapter;
 
     public MainActivityFragment() {
 
@@ -19,7 +29,9 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
+        mPosterAdapter= new PosterAdapter(getActivity(), Arrays.asList(list));
+        ListView curr = (ListView) rootView.findViewById(R.id.moviePosterList);
+        curr.setAdapter(mPosterAdapter);
         return rootView;
     }
 }
