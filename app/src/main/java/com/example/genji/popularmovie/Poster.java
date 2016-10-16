040a5990ad1dc046b9c6a207e5a0b71b9e788eb7
c26842684 <d26842684@hotmail.com>
2016-10-16 17:28:05 -0400

    poster Class

---
+++ app/src/main/java/com/example/genji/popularmovie/Poster.java
@@ -0,0 +1,19 @@
+package com.example.genji.popularmovie;
+
+
+public class Poster {
+    public int image;
+    public String name;
+    public String releaseDate;
+    public double rate;
+    public String desc;
+
+    public Poster(int image,String name,String releaseDate,double rate,String desc){
+        this.image = image;
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.rate = rate;
+        this.desc = desc;
+
+    }
+}
